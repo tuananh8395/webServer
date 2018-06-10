dab406f255ebb24fc4c21e4bbb6ad44986892f98
Bui Tuan Anh <tuananh8395@gmail.com>
2018-06-10 15:49:20 +0700

    Update webServer

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var count = 0
 
 http.createServer(function(req, res) {
 	count = count + 1
-	console.log("login")
+	console.log(res.url)
 	res.writeHead(200, {'Content-Type':'text/html'})
-	// var data = fs.readFileSync(__dirname + "/index.html", "utf-8")
+	var data = fs.readFileSync(__dirname + "/index.html", "utf-8")
 	// data = data.replace("{count}", count)
-	res.end("count = " + count)
-}).listen(7777)
+	res.end(data)
+}).listen(3000)

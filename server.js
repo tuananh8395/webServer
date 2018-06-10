0e5e03266ae6eb1c7adab03a728f07e043be4d2a
Bui Tuan Anh <tuananh8395@gmail.com>
2018-06-10 16:05:12 +0700

    Update webServer

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var http = require('http');
-// var fs = require("fs")
+var fs = require("fs")
 
 var count = 0
 
 http.createServer(function(req, res) {
 	count = count + 1
-	console.log(res.url)
+	console.log('Accessing to Server')
+	var ip = req.connection.remoteAddress;
+	console.log('IP: ' + ip);
 	res.writeHead(200, {'Content-Type':'text/html'})
 	var data = fs.readFileSync(__dirname + "/index.html", "utf-8")
 	// data = data.replace("{count}", count)
